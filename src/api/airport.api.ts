8637e38cb9103cf6f275a64221301f98cd38978d
Alan Wernick <alan.wernik@gmail.com>
2018-01-27 18:37:16 -0600

    Implement airport api forwarder

--- src/api/airport.api.ts
+++ src/api/airport.api.ts
@@ -3,7 +3,29 @@
 //
 
 import { Request, Response } from "express";
+import * as http from "request";
+import * as config from "../config";
+import * as qs from "querystring";
+
+const paths = {
+  index: '/airports',
+}
 
 export function index(req: Request, res: Response) {
+  const options = {
+    url: config.engineUrl + paths.index,
+    qs: req.query,
+  }
+
+  http.get(options, (err, response) => {
+    if(err) {
+      return res.sendStatus(500);
+    }
+    
+    return res
+      .status(response.statusCode)
+      .send(response.body);
+  });
 }
+
 
