054656c15bd4e5b537164a04295a38bcfea0428d
Alan Wernick <alan.wernik@gmail.com>
2018-01-28 00:13:39 -0600

    Adding firebase

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,13 @@ var port = process.env.PORT || 3000;
 var app = express();
 var api = new api_1.Api();
 var hook = new hook_1.GoogleAssistantHook();
+var config = {
+    apiKey: "<API_KEY>",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+};
 app.use(bodyparser.json());
+app.use(function (req, res) {
+});
 app.use('/', api.router);
 app.use('/hook', hook.router);
 app.listen(port, function () { return console.log("AA Backend started on port " + port); });
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,21 @@ import * as express from "express";
 import { Api } from "./api";
 import { GoogleAssistantHook } from "./hook";
 import * as bodyparser from "body-parser";
+import * as firebase from "firebase";
 
 const port = process.env.PORT || 3000;
 const app = express();
 const api = new Api();
 const hook = new GoogleAssistantHook();
 
+const config = {
+  apiKey: "<API_KEY>",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+}
+
 app.use(bodyparser.json());
+app.use((req, res) => {
+})
 app.use('/', api.router);
 app.use('/hook', hook.router);
 app.listen(port, () => console.log(`AA Backend started on port ${port}`));
